eb89ef4e8bb79b5fa86424d45ff0dd3d30c8a44e
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-03-26 15:49:26 +0800

    Added emptywallet and idl

--- emptyWallet.ts
+++ emptyWallet.ts
@@ -20,13 +20,11 @@ const connection = new Connection("https://api.devnet.solana.com");
     // Get balance of dev wallet
     const balance = await connection.getBalance(from.publicKey);
 
-    // Initially we do a transaction of 0.01 sol
     const transaction = new Transaction().add(
-      // transfer 0.01 SOL
       SystemProgram.transfer({
         fromPubkey: from.publicKey,
         toPubkey: to,
-        lamports: 0.01 * LAMPORTS_PER_SOL,
+        lamports: balance,
       })
     );
 
@@ -36,13 +34,33 @@ const connection = new Connection("https://api.devnet.solana.com");
 
     transaction.feePayer = from.publicKey;
 
+    // Calculate the exact fee rate to transfer entire SOL amount out of account minus fees
+    const fee =
+      (
+        await connection.getFeeForMessage(
+          transaction.compileMessage(),
+          "confirmed"
+        )
+      ).value || 0;
+
+    // Remove our transfer instruction to replace it
+    transaction.instructions.pop();
+
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance - fee,
+      })
+    );
+
     // Sign transaction, broadcast and confirm
     const signature = await sendAndConfirmTransaction(connection, transaction, [
       from,
     ]);
 
     console.log(`Success! Check out your TX here:
-          https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+        https://explorer.solana.com/tx/${signature}?cluster=devnet`);
   } catch (e) {
     console.error(`Oops something went wrong: ${e}`);
   }
